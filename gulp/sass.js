04461734f2c27458ac769e7b38f9e888b1f416ba
adotellison <adotellison@gmail.com>
2017-06-13 17:55:25 +0100

    Update design page for sprint4 to hide and show list titles correctly

--- gulp/sass.js
+++ gulp/sass.js
@@ -120,3 +120,25 @@ gulp.task('sass-documentation-S3', function () {
   .pipe(sourcemaps.write())
   .pipe(gulp.dest(config.paths.publicS3 + '/stylesheets/'))
 })
+
+gulp.task('sass-S4', function () {
+  return gulp.src(config.paths.assetsS4 + '/sass/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass({outputStyle: 'expanded',
+    includePaths: ['govuk_modules/govuk_frontend_toolkit/stylesheets',
+      'govuk_modules/govuk_template/assets/stylesheets',
+      'govuk_modules/govuk-elements-sass/']}).on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(config.paths.publicS4 + '/stylesheets/'))
+})
+
+gulp.task('sass-documentation-S4', function () {
+  return gulp.src(config.paths.docsAssetsS4 + '/sass/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass({outputStyle: 'expanded',
+    includePaths: ['govuk_modules/govuk_frontend_toolkit/stylesheets',
+      'govuk_modules/govuk_template/assets/stylesheets',
+      'govuk_modules/govuk-elements-sass/']}).on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(config.paths.publicS4 + '/stylesheets/'))
+})
